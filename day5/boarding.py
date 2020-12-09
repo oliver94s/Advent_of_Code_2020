133a0df7d2f241f31d15418c79b72a733157b520
Oliver Kim <oliver.santi.kim@gmail.com>
2020-12-08 21:34:42 -0800

    finished

--- day5/boarding.py
+++ day5/boarding.py
@@ -1,6 +1,6 @@
 import argparse
 import copy
-
+import math
 
 def parse_input(input_file):
     """
@@ -9,37 +9,100 @@ def parse_input(input_file):
     """
     rtn_list = []
     with open(input_file, 'r') as f:
-        passport = {}
         for line in f.readlines():
-            if line == "\n":
-                rtn_list.append(copy.copy(passport))
-                passport = {}
-                continue
-
-            split_spaces = line.split(' ')
-
-            for space in split_spaces:
-                k, v = space.split(':')
-                passport[k] = v.strip()
-
+            rtn_list.append(line.strip())
     return rtn_list
 
 
-def problem_1(passports):
+def problem_1(seating):
     """
 
     """
     valid_pass = 0
-    req_fields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
-    
-    for entry in passports:
-        fields = list(entry.keys())
-        if 'cid' in fields:
-            fields.remove('cid')
-        if len(fields) == len(req_fields):
-            valid_pass += 1
 
-    print(valid_pass)
+    highest = 0
+
+    row_max = 127
+    col_max = 7
+
+    for seat in seating:
+        lower = 0
+        upper = row_max
+        for pos in seat[:-3]:
+            if pos == "B":
+                lower = math.ceil(upper - ((upper - lower) / 2))
+            elif pos == "F":
+                upper = int((upper + lower) / 2)
+            else:
+                raise ValueError("%s uhh should not be here" % pos)
+        row = upper
+
+        lower = 0
+        upper = col_max
+        for pos in seat[-3:]:
+            if pos == "R":
+                lower = math.ceil(upper - ((upper - lower) / 2))
+            elif pos == "L":
+                upper = int((upper + lower) / 2)
+            else:
+                raise ValueError("%s uhh should not be here" % pos)
+        col = upper
+
+        id = row * 8 + col
+        if highest < id:
+            highest = id
+    print(highest)
+    return valid_pass
+
+def problem_2(seating):
+    """
+
+    """
+    valid_pass = 0
+
+    highest = 0
+
+    row_max = 127
+    col_max = 7
+    taken_seats = []
+    for seat in seating:
+        lower = 0
+        upper = row_max
+        for pos in seat[:-3]:
+            if pos == "B":
+                lower = math.ceil(upper - ((upper - lower) / 2))
+            elif pos == "F":
+                upper = int((upper + lower) / 2)
+            else:
+                raise ValueError("%s uhh should not be here" % pos)
+        row = upper
+
+        lower = 0
+        upper = col_max
+        for pos in seat[-3:]:
+            if pos == "R":
+                lower = math.ceil(upper - ((upper - lower) / 2))
+            elif pos == "L":
+                upper = int((upper + lower) / 2)
+            else:
+                raise ValueError("%s uhh should not be here" % pos)
+        col = upper
+
+        id = row * 8 + col
+        taken_seats.append(id)
+
+    taken_seats.sort()
+    
+    y = [str(x) for x in taken_seats]
+    print("\n".join(y))
+
+    for idx in range(0, len(taken_seats)-1):
+        if taken_seats[idx] <= 7:
+            continue
+        if taken_seats[idx] + 2 == taken_seats[idx+1]:
+            print(taken_seats[idx])
+
+
     return valid_pass
 
 
@@ -49,5 +112,6 @@ if __name__ == "__main__":
     
     args = ap.parse_args()
 
-    passports = parse_input(args.input_file)
-    problem_1(passports)
+    seating = parse_input(args.input_file)
+    # problem_1(seating)
+    problem_2(seating)
