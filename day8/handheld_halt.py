fc0257453ecf79e0dec2ff15528545531393b594
Oliver Kim <oliver.santi.kim@gmail.com>
2020-12-09 22:49:55 -0800

    finished part 2

--- day8/handheld_halt.py
+++ day8/handheld_halt.py
@@ -1,5 +1,6 @@
 import argparse
 import copy
+import json
 import math
 
 
@@ -42,29 +43,87 @@ def problem_1(commands):
     print(accumulator)
 
 
+def check(cmd_stack, commands):
+    while cmd_stack:
+        cmd, ran_cmds, starting = cmd_stack.pop()
+        print(ran_cmds, starting)
+        print("do stuff %s" % len(commands))
+        action = cmd[0]
+        if action == 'nop' or action == 'jmp':
+            if action == 'nop':
+                action = 'jmp'
+            else:
+                action = 'nop'
+            commands[starting][0] = action
+            if do_i_end(starting, ran_cmds, commands):
+                print("found the problem at %s" % starting)
+                return commands
 
-def problem_2(answers):
+def do_i_end(starting_line, ran_cmds, commands):
     """
 
     """
-    total = 0
-    for ans in answers:
-        print(ans)
-        people = ans['people']
-        valid = True
-        count = 0
-        for letter, val in ans.items():
-            if letter == "people":
-                continue
+    accumulator = 0
+    idx = starting_line
+    valid = True
+    while idx < len(commands):
+        cmd = commands[idx]
+        
+        if idx not in ran_cmds:
+            ran_cmds[idx] = 1
+        else:
+            ran_cmds[idx] += 1
+            valid = False
+            break
 
-            if val == people:
-                count += 1
+        action = cmd[0]
+        value = int(cmd[1])
+        if action == 'acc':
+            accumulator += value
+        
+        if action == 'jmp':
+            idx += value
+        else:
+            idx += 1
 
-        print(count)
-        total += count
+    # print(accumulator)
+    print(json.dumps(ran_cmds, indent=4))
+    return valid
 
-    print(total)
-    return total
+def problem_2(commands):
+    """
+
+    """
+    accumulator = 0
+    idx = 0
+    ran_cmds = {}
+    cmd_stack = []
+    while idx < len(commands):
+        cmd = commands[idx]
+        cmd_stack.append((cmd, copy.copy(ran_cmds), idx))
+        
+        if idx not in ran_cmds:
+            ran_cmds[idx] = 1
+        else:
+            cmd_stack.pop()
+            new_command = check(cmd_stack, commands)
+            print(json.dumps(new_command, indent=4))
+            print("broken")
+            problem_2(new_command)
+            break
+        
+
+        action = cmd[0]
+        value = int(cmd[1])
+        if action == 'acc':
+            accumulator += value
+        
+        if action == 'jmp':
+            idx += value
+        else:
+            idx += 1
+
+    print(accumulator)
 
 if __name__ == "__main__":
     ap = argparse.ArgumentParser("Day 6: Custom Customs")
@@ -74,4 +133,4 @@ if __name__ == "__main__":
 
     cmds = parse_input(args.input_file)
     problem_1(cmds)
-    # problem_2(answers)
+    problem_2(cmds)
