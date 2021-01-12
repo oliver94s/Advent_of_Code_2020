f610388ed60767a7e42d104d52781a0ecd581e7a
Oliver Kim <oliver.santi.kim@gmail.com>
2021-01-12 01:08:41 -0800

    i give up for the moment

--- day10/adapter_array.py
+++ day10/adapter_array.py
@@ -17,7 +17,7 @@ def parse_input(input_file):
     return rtn_list
 
 
-def is_valid(jolts, highest_jolts):
+def is_valid(jolts, highest_jolts=None):
     # if max(jolts) + 3 != highest_jolts:
     #     # print(max(jolts) + 3)
     #     return False
@@ -59,21 +59,95 @@ def do_stuff(jolts, candidates, highest_jolts):
         jolts_copy = copy.copy(jolts)
 
 
+def is_valid_2(jolts, highest_jolts=None):
+    if max(jolts) + 3 != highest_jolts and highest_jolts is not None:
+        return False
+
+    for idx in range(len(jolts)-1):
+        if jolts[idx+1] - jolts[idx] > 3:
+            return False
+    return True
+
+
+def find_candidates_2(jolts):
+    candidates = []
+
+    for idx in range(len(jolts)-1):
+        if jolts[idx+1] - jolts[idx] < 3:
+            candidates.append(jolts[idx+1])
+
+    return candidates
+
+
+def do_stuff_2(jolts, candidates, high):
+    global count
+    jolts_copy = copy.copy(jolts)
+    can_copy = copy.copy(candidates)
+
+    for candidate in candidates:
+        jolts_copy.remove(candidate)
+        
+        if is_valid_2(jolts_copy, high):
+            # print(jolts_copy[0:3], len(jolts_copy))
+            can_copy.remove(candidate)
+            count += 1
+            do_stuff_2(jolts_copy, can_copy, high)
+        jolts_copy = copy.copy(jolts)
+
+
+def split_candidate(jolts):
+    candidates = []
+    candidate = []
+    prev_jolt = 0
+    sub = 0
+    restart = False
+    for jolt in jolts:
+        if restart:
+            sub = jolt
+            restart = 0
+        jolt -= sub
+        candidate.append(jolt)
+        if jolt - prev_jolt > 2:
+            candidates.append(candidate)
+            candidate = []
+            # print(jolt + sub)
+            restart = True
+
+        prev_jolt = jolt
+    print(candidates)
+    return candidates
+
+
 
 def problem_2(jolts):
     global count
     jolts.sort()
-    with open("foo.txt", 'w') as f:
-        for jolt in jolts:
-            f.write(str(jolt) + '\n')
-    highest_jolts = max(jolts) + 3
-    print(len(jolts))
-    # print(highest_jolts)
-    # print(jolts)
-    candidates = find_candidates(jolts)
-    print(candidates, len(candidates))
-    do_stuff(jolts, candidates, highest_jolts)
-    print("total: %s" % count)
+    jolts.insert(0,0)
+    things = split_candidate(jolts)
+    product = 1
+    for thing in things:
+        highest = max(thing) + 3
+        candidates = find_candidates(thing)
+        do_stuff_2(thing, candidates, highest)
+        product *= count
+        print(count)
+        count = 1
+        
+    print(product)
+
+
+
+    # with open("foo.txt", 'w') as f:
+    #     for jolt in jolts:
+    #         f.write(str(jolt) + '\n')
+    # highest_jolts = max(jolts) + 3
+    # print(len(jolts))
+    # # print(highest_jolts)
+    # # print(jolts)
+    # candidates = find_candidates(jolts)
+    # print(candidates, len(candidates))
+    # do_stuff(jolts, candidates, highest_jolts)
+    # print("total: %s" % count)
 
 
 if __name__ == "__main__":
